2ea75fa9812b1ac5b9649e39354835be7b6899fc
Neo7411 <davidangi02@gmail.com>
2023-10-14 14:22:10 +0200

    test of classes

--- src/main/java/com/Foodora/Volt/VoltApplication.java
+++ src/main/java/com/Foodora/Volt/VoltApplication.java
@@ -18,6 +18,12 @@ public class VoltApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(VoltApplication.class, args);
-		
+		Food d1= new Food(1,"Roman Kenyer",750.0,4.5,false,false);
+		Food d2= new Food(2,"Mirelit Pizza",150.0,1.0,true,true);
+		Courier cr7= new Courier(1,true,"KicsiKacsa",321,"0683424423","foot",1.0);
+		Customer c1= new Customer(3,"chiwawaankle",5,"06423435324", "Virag utca 2",200,true );
+		Order od1= new Order(1,2,3,"312413231");
+		System.out.println(d1);
+		System.out.println(d2);
 	}
 }
