f858e818c820f40f00cb28f0d3c131749f4905aa
Diklol-cig <63556954+Diklol-cig@users.noreply.github.com>
2023-11-13 10:10:46 +0100

    Classes Entities placeholder frontend

---
+++ Projekt basics/src/main/java/hu/unideb/bootstrap_test/model/Orders.java
@@ -0,0 +1,28 @@
+package hu.unideb.bootstrap_test.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orders {
+    @Id
+    @GeneratedValue
+    private Integer id; //Order ID
+    private Integer userId;
+    private Integer CourierId;
+    private String orderStatus;
+    private String deliveryAddress;
+    private Integer orderTimeStamp;
+    private Integer totalPrice;
+}
