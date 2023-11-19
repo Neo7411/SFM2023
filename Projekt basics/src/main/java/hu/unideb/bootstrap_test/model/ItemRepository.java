65b8c6d03797eff2459f0475c87d4a6f3b2e7bf4
PATRIK2002PP <patrik2002pp@gmail.com>
2023-11-19 16:00:17 +0100

    figma to react login.js

--- Projekt basics/src/main/java/hu/unideb/bootstrap_test/model/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package hu.unideb.bootstrap_test.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item,Integer> {
-}
--- Projekt basics/src/main/java/hu/unideb/bootstrap_test/model/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.unideb.bootstrap_test.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
-
